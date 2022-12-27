6bbf979b3f48b87dce2edef419556f2cd4cb99c0
Lucas <lucascor_rtb@hotmail.com>
2022-12-27 11:17:32 -0400

    fix(emailjs): bug fix

--- src/services/ipconfig.ts
+++ src/services/ipconfig.ts
@@ -2,13 +2,18 @@ import axios from 'axios'
 import emailjs from 'emailjs-com'
 
 export async function getIp() {
-    const IPDATALOCALSTORAGE = sessionStorage.getItem("IP_DATA_CLIENT");
+    console.log("ENTROU NO getIP")
+    const IPDATALOCALSTORAGE = localStorage.getItem("IP_DATA_CLIENT");
     if(IPDATALOCALSTORAGE) {
+        console.log("ENTROU NO IF getIP", IPDATALOCALSTORAGE)
         return;
-     }
+    }
+    const ip = await axios.get("https://api.ipify.org")
+    console.log(ip.data)
     const key = 'f42c6e9e';
-    const { data } = await axios.get(`https://api.hgbrasil.com/geoip?format=json-cors&key=${key}&address=remote&fields=only_results,city,region,country_name,continent&precision=false`);
-    sessionStorage.setItem("IP_DATA_CLIENT", JSON.stringify(data));
+    const { data } = await axios.get(`https://api.hgbrasil.com/geoip?format=json-cors&key=${key}&address=${ip.data}&fields=only_results,city,region,country_name,continent&precision=false`);
+    console.log("PEGOU O IP", data)
+    localStorage.setItem("IP_DATA_CLIENT", JSON.stringify(data));
     emailjs.send('market_easy', 'template_ao8ucsp', {
         cidade: data.city, 
         continente: data.continent, 
